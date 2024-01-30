e78193f6cbd3796395ff74a47732308058a96e94
Miaro Andy <miaroandy25@gmail.com>
2024-01-30 21:21:52 +0300

    add angular@cli

--- Front-end/src/app/app.routes.ts
+++ Front-end/src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import { RouterModule,Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes, { useHash: true })],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
