3b47d1a96f676d3f5b055fded5bf886c264d0f9a
RAJAOFERA Maharo Steven <100205337+StevenRaj03@users.noreply.github.com>
2024-02-01 20:28:23 +0300

    [Back] service CRUD

--- Back-end/app.js
+++ Back-end/app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var servicesRouter = require('./routes/services');
 
 var app = express();
 
@@ -17,5 +18,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/services', servicesRouter);
 
 module.exports = app;
