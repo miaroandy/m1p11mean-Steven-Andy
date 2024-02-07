c9c0a8fe102739865ab9f2f82687b3575b9c08fe
Miaro Andy <miaroandy25@gmail.com>
2024-02-07 18:56:29 +0300

    init back end url

--- Front-end/src/app/utilitaires/CallAPI.ts
+++ Front-end/src/app/utilitaires/CallAPI.ts
@@ -9,7 +9,7 @@ import { Observable,tap,catchError,of } from 'rxjs';
     providedIn: 'root',
 })
 export class CallAPI {
-    apiUrl = 'http://localhost:3000/';
+    apiUrl = 'https://salon-beaute-service.onrender.com/';
 
     constructor(private http: HttpClient) { }
 
