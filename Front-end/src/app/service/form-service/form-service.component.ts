d4549ec9b0bbccba5ea26226f499ed6433d7f32a
RAJAOFERA Maharo Steven <100205337+StevenRaj03@users.noreply.github.com>
2024-02-11 21:34:56 +0300

    [Front] update gestion service

--- Front-end/src/app/service/form-service/form-service.component.ts
+++ Front-end/src/app/service/form-service/form-service.component.ts
@@ -5,39 +5,38 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { Service } from '../../model/Service';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 import { Router } from '@angular/router';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-form-service',
   standalone: true,
-  imports: [NzInputModule, FormsModule, NzButtonModule, NzGridModule],
+  imports: [NzInputModule, NzTypographyModule, FormsModule, NzButtonModule, NzGridModule, NgIf, NzSpinModule],
   templateUrl: './form-service.component.html',
   styleUrl: './form-service.component.css'
 })
 export class FormServiceComponent {
   service: Service = new Service();
+  loading = false;
 
   constructor(
     private callAPI: CallAPI,
     private router: Router
   ) { }
 
-  reloadComponent() {
-    this.router.navigateByUrl('/welcome', { skipLocationChange: true }).then(() => {
-      this.router.navigate(['welcome']);
-    });
-  }
-
-  onSubmit(){
+  onClick(){
+    this.loading = true;
     this.callAPI.saveService(this.service).subscribe(
       (data: any) => {
-        this.reloadComponent()
+        this.router.navigate(['/employe'])
+        this.loading=false;
       },
       (error: any) => {
         // Gérez les erreurs ici
         console.error('Erreur', error);
       }
     );
-    return false;
   }
 }
--- Front-end/src/app/service/list-service/list-service.component.ts
+++ Front-end/src/app/service/list-service/list-service.component.ts
@@ -1,37 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Service } from '../../model/Service';
 import { NzTableModule } from 'ng-zorro-antd/table';
 import { NgFor,NgIf } from '@angular/common';
-import { NzInputModule } from 'ng-zorro-antd/input';
-import { CallAPI } from '../../utilitaires/CallAPI';
 import { NzSpinModule } from 'ng-zorro-antd/spin';
-
+import { CallAPI } from '../../utilitaires/CallAPI';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-list-service',
   standalone: true,
-  imports: [NzTableModule, NgFor, NzInputModule, NgIf, NzSpinModule],
+  imports: [NzTableModule, NzTypographyModule, NgFor, NgIf, NzSpinModule, NzButtonModule],
   templateUrl: './list-service.component.html',
   styleUrl: './list-service.component.css',
-  providers: [CallAPI]
+  providers: []
 })
-export class ListServiceComponent {
+export class ListServiceComponent implements OnInit {
   services: Service[] = [];
   nbrServices: number = 0;
+  loading=true;
 
-  constructor(private callAPI: CallAPI) { }
+  constructor(private callAPI: CallAPI,
+    private router: Router
+  ) { }
 
   ngOnInit() {
-    this.callAPI.getAllServices().subscribe(
-      (data) => {
-        this.services = data;
-        this.nbrServices = this.services.length;
-      },
-      (error) => {
-        // Gérez les erreurs ici
-        console.error('Erreur lors de la récupération des données de l\'API', error);
-      }
-    );
+    this.callAPI.getAllServices().subscribe(result => {
+      this.services = result;
+      this.nbrServices = this.services.length;
+      this.loading=false;
+    });
   }
 
+  onClick(id: string){
+    this.router.navigate(['/service', id])
+  }
 }
