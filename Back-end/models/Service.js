2ee2381fb2bf8981dd10e89aa20c4f7a9b97cd19
RAJAOFERA Maharo Steven <100205337+StevenRaj03@users.noreply.github.com>
2024-02-12 09:26:45 +0300

    [Back] add photo

--- Back-end/models/Service.js
+++ Back-end/models/Service.js
@@ -1,10 +1,18 @@
 const mongoose = require('mongoose');
 
+const offresSpeciales = new mongoose.Schema({
+  datedebut: { type: Date, required: true },
+  datefin: { type: Date, required: true },
+  reduction: { type: Number, required: true },
+})
+
 const serviceSchema = new mongoose.Schema({
   nom: { type: String, required: true },
   prix: { type: Number, required: true },
   duree: { type: Number },
   taux_commission: { type: Number },
+  photo: { type: String },
+  offres_speciales: [offresSpeciales]
 });
 
 const Service = mongoose.model('Service', serviceSchema, 'services_collection');
