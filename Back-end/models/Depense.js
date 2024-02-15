c7a2a817a4e8633c11bc4249d83941d8cacf660e
RAJAOFERA Maharo Steven <100205337+StevenRaj03@users.noreply.github.com>
2024-02-15 10:30:53 +0300

    [Back] Update depense routes & model

--- Back-end/models/Depense.js
+++ Back-end/models/Depense.js
@@ -4,6 +4,7 @@ require('./Manager');
 const depenseSchema = new mongoose.Schema({
     manager: { type: mongoose.Schema.Types.ObjectId , ref: 'Manager' },
     salaire: { type: Number },
+    achat_piece: { type: Number },
     loyer: { type: Number },
     autres_depenses: { type: Number },  // Ne pas renvoyer le mot de passe par défaut
     date: { type: Date }
