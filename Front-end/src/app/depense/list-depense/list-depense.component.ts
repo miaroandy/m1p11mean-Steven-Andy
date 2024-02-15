91ae459c0fcde074be2c466fea1b818ee97fb146
RAJAOFERA Maharo Steven <100205337+StevenRaj03@users.noreply.github.com>
2024-02-15 10:35:16 +0300

    [Front] Liste depense

---
+++ Front-end/src/app/depense/list-depense/list-depense.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Depense } from '../../model/Depense';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NgFor,NgIf } from '@angular/common';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { CallAPI } from '../../utilitaires/CallAPI';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-list-depense',
+  standalone: true,
+  imports: [NzTableModule, NzTypographyModule, NgFor, NgIf, NzSpinModule, NzButtonModule],
+  templateUrl: './list-depense.component.html',
+  styleUrl: './list-depense.component.css',
+  providers: []
+})
+export class ListDepenseComponent implements OnInit {
+  depenses: Depense[] = [];
+  nbrDepenses: number = 0;
+  loading=true;
+
+  constructor(private callAPI: CallAPI,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.callAPI.getAllDepenses().subscribe(result => {
+      this.depenses = result;
+      this.nbrDepenses = this.depenses.length;
+      this.loading=false;
+    });
+  }
+
+  onClick(id: string){
+    this.router.navigate(['/depense', id])
+  }
+}
